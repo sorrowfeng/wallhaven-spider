de91c4cdc2e0b1bca7d1b5aa59b21d1fa564e39d
sorrowfeng <1399600304@qq.com>
2022-06-24 23:48:20 +0800

    update settings

--- spider_thread.py
+++ spider_thread.py
@@ -2,6 +2,24 @@ import requests      #爬虫
 import re            #正则表达式
 import os            #文件操作
 import threading     #线程
+import time
+
+
+threads = []
+
+header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44'
+}  # 模拟浏览器头部，伪装成用户
+
+
+class myThread (threading.Thread):
+    def __init__(self, callback_func, url):
+        threading.Thread.__init__(self)
+        self.download_image = callback_func
+        self.img_path = url
+
+    def run(self):
+        self.download_image(self.img_path)
 
 
 class Spider(threading.Thread):
@@ -29,9 +47,8 @@ class Spider(threading.Thread):
         return base_url         #返回模板
 
     def get_img_url(self, base_url):
-        header={
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74'
-        }  #模拟浏览器头部，伪装成用户
+        self.error_num = 0
+        self.finish_num = 0
         img_url_list=[]  #创建一个空列表
         page_num=2 #"请输入下载页数:(一页24张)"
         now_num=1
@@ -42,7 +59,8 @@ class Spider(threading.Thread):
                 return []
             new_url=base_url+str(now_num)   #将模板进行拼接得到每页壁纸完整的url(实质:字符串的拼接)
             page_text=requests.get(url=new_url,headers=header).text #获取url源代码
-            ex='<a class="preview" href="(.*?)"'
+            # ex='<a class="preview" href="(.*?)"'
+            ex='<img alt="loading" class="lazyload" data-src="(.*?)"'
             img_url_list+=re.findall(ex,page_text,re.S) #利用正则表达式从源代码中截取每张壁纸缩略图的url并全部存放在一个列表中
             if now_num==2:
                 expage = '<span class="thumb-listing-page-num">2</span> / (.*?)</h2>'
@@ -56,26 +74,29 @@ class Spider(threading.Thread):
             now_num += 1
             if now_num > 2:
                 self.update_progress(float(now_num) / float(page_num) * 100.0)
-
         return img_url_list #返回列表
+
+    def download_urllist(self, new_url):
+        page_text = requests.get(url=new_url, headers=header).text  # 获取url源代码
+        # ex='<a class="preview" href="(.*?)"'
+        ex = '<img alt="loading" class="lazyload" data-src="(.*?)"'
+        # img_url_list += re.findall(ex, page_text, re.S)  # 利用正则表达式从源代码中截取每张壁纸缩略图的url并全部存放在一个列表中
 
 
     def download_img(self, img_url_list):
+        print(img_url_list)
         if len(img_url_list) == 0:
             return
-
-        header={
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
-        } #模拟浏览器头部，伪装成用户
-        if not os.path.exists('./wallpapers'):     #在目录下创建一个名为wallpapers的文件夹
-            os.mkdir('./wallpapers')
-        path='./wallpapers/'+self.keyword
-        self.send_message("图片下载文件夹:"+path)
+        print(self.download_path)
+        if not os.path.exists(self.download_path):     #在目录下创建文件夹
+            os.mkdir(self.download_path)
+        self.path=self.download_path+'/'+self.keyword
+        self.send_message("图片下载文件夹:"+self.path)
         self.send_message("开始下载")
         self.update_progress(0)
 
-        if not os.path.exists(path):#在wallpapers文件夹下创建一个以关键词命名的子文件夹以存放此次下载的所有壁纸
-            os.mkdir(path)
+        if not os.path.exists(self.path):#在wallpapers文件夹下创建一个以关键词命名的子文件夹以存放此次下载的所有壁纸
+            os.mkdir(self.path)
         for i in range(len(img_url_list)): #循环遍历列表，对每张壁纸缩略图的url进行字符串的增删获得壁纸原图下载的url  注：jpg或png结尾
             if self.exit:
                 self.update_progress(0)
@@ -83,20 +104,58 @@ class Spider(threading.Thread):
                 return
             x=img_url_list[i].split('/')[-1]  #获取最后一个斜杠后面的字符串
             a=x[0]+x[1] #获取字符串的前两位
-            img_url='https://w.wallhaven.cc/full/'+a+'/wallhaven-'+x+'.jpg'  #拼接字符串,先默认jpg结尾
-            code=requests.get(url=img_url,headers=header).status_code
-            if code==404:    #若网页返回值为404，则为png结尾
-                img_url='https://w.wallhaven.cc/full/'+a+'/wallhaven-'+x+'.png'
-            img_data=requests.get(url=img_url,headers=header,timeout=20).content  #获取壁纸图片的二进制数据,加入timeout限制请求时间
-            img_name=self.keyword+'-'+img_url.split('-')[-1] #生成图片名字
-            img_path=path+'/'+img_name    #生成图片存储路径
-            with open(img_path,'wb') as fp:    #('w':写入,'b':二进制格式)
-                fp.write(img_data)
-                self.send_message('['+str(i)+'/'+str(len(img_url_list))+']\t'+img_name+'下载成功')     #每张图片下载成功后提示
-            self.update_progress(float(i) / float(len(img_url_list)) * 100.0)
+            img_url='https://w.wallhaven.cc/full/'+a+'/wallhaven-'+x
+            # self.download_one_image(img_base_url)
+
+            self.total_num = len(img_url_list)
+            thread_num = myThread(self.download_one_image, img_url)
+            time.sleep(0.015)    # 加入延迟
+            thread_num.start()
+            # 添加线程到线程列表
+            threads.append(thread_num)
+            time.sleep(0.015)    # 加入延迟
+
+
+        while not threads:
+            pass
+
+        for t in threads:
+            time.sleep(0.01)
+            # print(t)
+            t.join()
+
+        self.send_message('下载结束!\n成功下载'+str(self.finish_num)+'张图片，'+str(self.error_num)+'张图片下载失败')
+
+
+    def download_one_image(self, img_url):
+        try:
+            image = requests.get(url=img_url, headers=header, timeout=20)
+            code = image.status_code
+            if code == 200:
+                img_data = image.content  # 获取壁纸图片的二进制数据,加入timeout限制请求时间
+                img_name = self.keyword + '-' + img_url.split('-')[-1]  # 生成图片名字
+                img_path = self.path + '/' + img_name  # 生成图片存储路径
+                with open(img_path, 'wb') as fp:  # ('w':写入,'b':二进制格式)
+                    fp.write(img_data)
+                self.finish_num += 1
+
+                self.send_message('已下载\t[' + str(self.finish_num) + '/' + str(self.total_num) + ']')  # 每张图片下载成功后提示
+            else:
+                self.error_num += 1
+
+        except requests.exceptions.Timeout as e:
+            self.error_num += 1
+
+        self.update_progress(float(self.error_num + self.finish_num) / float(self.total_num) * 100.0)
+
 
     def set_word(self, word):
         self.keyword = word
+
+
+    def set_download_path(self, path):
+        self.download_path = path
+
 
     def run(self):
         self.exit = False
